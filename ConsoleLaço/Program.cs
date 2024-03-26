0a760d0a643c3cfb6ca12e4f34cede9c1d98e2b7
Usuario <Usuario@CPD02-M04>
2024-03-26 11:21:50 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleLaço/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLaço
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de multiplicações: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tabuada do:" + numero);
+            for (int i = 1; i <= quantidade; i++) ;
+            {
+                int resultado numero * i;
+                Console.WriteLine($"{numero} X {i} = {resultado}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
